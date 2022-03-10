532829b1be3862777c65535f6c14d3c18d8e26b0
carlos <cf8838@gmail.com>
2022-03-09 23:56:01 -0500

    My first Commit

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ var x = document.getElementById("speedDisplay");
 
 function getLocation() {
   if (navigator.geolocation) {
-    navigator.geolocation.watchPosition(showPosition);
+    navigator.geolocation.getCurrentPosition(showPosition);
   } else {
     x.innerHTML = "Geolocation is not supported by this browser.";
   }
 }
 function showPosition(position) {
-  x.innerHTML = "" + position.coords.speed
+  x.innerHTML = "" + position.coords.altitude
 }
